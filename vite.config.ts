0092ee986992d5e0a9ed1d7cc4b3d0448826244e
Rafael Pontes <rafafpontes@gmail.com>
2025-03-24 13:35:22 -0300

    fix: corrige configuração de build usando esbuild

--- vite.config.ts
+++ vite.config.ts
@@ -8,13 +8,7 @@ export default defineConfig({
     exclude: ['lucide-react'],
   },
   build: {
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true
-      }
-    },
+    minify: 'esbuild',
     rollupOptions: {
       output: {
         manualChunks: {
